0dab2358d9cbcb9bbd5a9ba6be3a40341908dd6d
KurochkinAV <kurochkin_013@list.ru>
2022-09-07 17:43:23 +0300

    DZ_16 наконец-то

--- main_flska.py
+++ main_flska.py
@@ -1,35 +1,33 @@
+import pprint
 from flask import Flask, render_template, request
 from main import parce
 
-
+# объявление главной переменной
 app = Flask(__name__)
 
 
-@app.route("/")
+# вывод (редеринг) главной страницы
+@app.get('/index')
+@app.get('/')
 def index():
     return render_template('index.html')
 
 
-# @app.get('/index')
-# @app.get('/')
-# def index():
-#     return render_template('index.html')
 
-
-@app.route('/forma/')
-def contacts():
+# вывод страницы формы
+@app.route('/form/')
+def form():
     return render_template('forma.html')
 
 
-@app.route('/result/')
-def results():
+@app.route('/result/', methods=['POST'])
+def result():
     vac = request.form
+    # pprint.pprint('GGGGGGGGGGGGgG', vac)
     data = parce(**vac)
-    dat = {**data, **vac}
+    dat = {**data, **vac}  # data | vac - в Python 3.10 можно сделать так
     print(dat)
     return render_template('contacts.html', res=dat)
-
-
 
 
 
