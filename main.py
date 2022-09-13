e8a6e13764985b749415c7fda7087330e53133b9
KurochkinAV <kurochkin_013@list.ru>
2022-09-13 13:16:42 +0300

    DZ_17

--- main.py
+++ main.py
@@ -6,63 +6,56 @@ from collections import Counter
 from os.path import exists
 from pickle import dump, load
 from pycbrf import ExchangeRates
-
+import sqlite3
+from sqlite3 import connect
 import requests
 
 def parce(vacancy, pages='3', where='all'):
+    """
+    Обработка данных по вакансиям, получение средней верхней и нижней границы зарплат
+     и процентного отношения для каждого навыка.
+    :param vacancy: текст поиска в вакансиях.
+    :param pages: количество анализируемых страниц.
+    :param where: место поиска текста.
+    :return: словарь с результатами анализа вакансий.
+    """
     url = 'https://api.hh.ru/vacancies'
-    # Загрузка текущих курсов ввалют
     rate = ExchangeRates()
-    # загрузка файла с цифровым кодом
-    if exists('area.txt'):
-        with open('area.txt', mode='rb') as f:
-            area = load(f)
-    else:
-        area = {}
     p = {'text': vacancy if where == 'all' else f'NAME: {vacancy}' if where == 'name' else f'COMPANY_NAME: {vacancy}'}
     r = requests.get(url=url, params=p).json()
-    # pprint.pprint(r) - успешно
-    # количество страниц
     count_pages = r['pages']
-    # вывод количества страниц по интересующей вакансии
-    # print(count_pages)
-    # количество вакансий на странице
     all_count = len(r['items'])
-    # print(all_count)
     result = {
-        'keywords': vacancy,
-        'count': all_count
-    }
-
-    # зароботная плата
+            'keywords': vacancy,
+            'count': all_count}
     sal = {'from': [], 'to': [], 'cur': []}
-    # ключеаые навыки
     skillis = []
-    # сначала выявляем сколько будет получено страниц
-    # и готовим нужные переменные. А затем проходим по каждой из полученных страниц
     for page in range(count_pages):
         if page > int(pages):
             break
         else:
-            print(f'Обрабатывается страница {page}')
-        # задаем параметры
+            print(f"Обрабатывается страница {page}")
         p = {'text': vacancy,
-             'page': page
-             }
+             'page': page}
         ress = requests.get(url=url, params=p).json()
         all_count = len(ress['items'])
         result['count'] += all_count
         for res in ress['items']:
-            # pprint.pprint(res)
+            # pprint(res)
             skills = set()
             city_vac = res['area']['name']
-    #         добавление города из ответа на запрос, если его нет в файле
-            if city_vac not in area:
-                area[city_vac] = res['area']['id']
+            con = connect('base.sqlite')
+            cur = con.cursor()
+            # проверка наличия города в таблице
+            rest = cur.execute('select id from area where area.name = ?', (city_vac,)).fetchone()
+            if not rest:
+                # добавление строки в таблицу регионов.
+                cur.execute('insert into area values (null, ?, ?)', (city_vac, res['area']['id']))
+                con.commit()
+            con.close()
             ar = res['area']
             res_full = requests.get(res['url']).json()
-            # pprint.pprint(res_full)
-    #         обработка описанеия вакансии
+            # pprint(res_full)
             pp = res_full['description']
             # print(pp)
             pp_re = re.findall(r'\s[A-Za-z-?]+', pp)
@@ -75,39 +68,29 @@ def parce(vacancy, pages='3', where='all'):
             for it in its:
                 if not any(it in x for x in skills):
                     skillis.append(it)
-    #окончание формирования списка навыков
-    # обработка зарплаты
             if res_full['salary']:
                 code = res_full['salary']['currency']
                 if rate[code] is None:
                     code = 'RUR'
                 k = 1 if code == 'RUR' else float(rate[code].value)
-                sal['from'].append(k * res_full['salary']['from']) if res['salary']['from'] else k * res_full['salary']['to']
-                sal['to'].append(k * res_full['salary']['to']) if res['salary']['to'] else k * res_full['salary']['from']
+                sal['from'].append(k * res_full['salary']['from'] if res['salary']['from'] else k * res_full['salary']['to'])
+                sal['to'].append(k * res_full['salary']['to'] if res['salary']['to'] else k*res_full['salary']['from'])
     # print(skillis)
     sk2 = Counter(skillis)
-    # pprint.pprint(sk2)
+    # pprint(sk2)
     up = sum(sal['from']) / len(sal['from'])
     down = sum(sal['to']) / len(sal['to'])
-    result.update(({'down': round(up, 2),
-                    'up': round(down, 2)}))
+    result.update({'down': round(up, 2),
+                   'up': round(down, 2)})
     add = []
     for name, count in sk2.most_common(5):
         add.append({'name': name,
                     'count': count,
-                    'percent': round((count / result['count']) * 100, 2)
-                    })
+                    'percent': round((count / result['count'])*100, 2)})
     result['requirements'] = add
-    with open('area.pkl', 'wb') as f:
-        dump(area, f)
     return result
 
 
 if __name__ == '__main__':
-    vacancy = input('Введеите интересующую вакансию: ')
-    pprint.pprint(parce(vacancy))
-
-
-
-
-
+    vacancy = input('Введите интересующую вакансию: ')
+    pprint(parce(vacancy))
--- main_flska.py
+++ main_flska.py
@@ -1,7 +1,8 @@
 import pprint
 from flask import Flask, render_template, request
 from main import parce
-
+from sqlite3 import connect
+from crud import add_row
 # объявление главной переменной
 app = Flask(__name__)
 
@@ -23,10 +24,11 @@ def form():
 @app.route('/result/', methods=['POST'])
 def result():
     vac = request.form
-    # pprint.pprint('GGGGGGGGGGGGgG', vac)
     data = parce(**vac)
     dat = {**data, **vac}  # data | vac - в Python 3.10 можно сделать так
-    print(dat)
+    print('orktgh0r9e-kher0h' , dat)
+    dat['where'] = 'в названии вакансии' if dat['where'] == 'name' else 'в названии компании' if dat['where'] == 'company' else 'везде'
+    add_row(dat)
     return render_template('contacts.html', res=dat)
 
 
